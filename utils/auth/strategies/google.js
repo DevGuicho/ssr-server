d0c9af980e1e8b214d80ee1df15cdfe2787b4ead
Luis Vazquez <luvazpa@gmail.com>
2021-01-20 01:52:50 -0600

    google auth added

---
+++ utils/auth/strategies/google.js
@@ -0,0 +1,32 @@
+const passport = require("passport");
+const { OAuth2Strategy: GoogleStrategy } = require("passport-google-oauth");
+
+const { config } = require("../../../config/index");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.googleClientId,
+      clientSecret: config.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      const { data, status } = await axios({
+      url: `${config.api.url}/api/auth/sign-provider`,
+      method: 'POST',
+      data: {
+        name: profile._json.name,
+        email: profile._json.email,
+        password: profile.id,
+        apiKeyToken: config.api.keyToken,
+      },
+    });
+
+      if (!data || status !== 200) {
+        return cb(boom.unauthorized(), false);
+      }
+
+      return cb(null, data);
+    }
+  )
+);
